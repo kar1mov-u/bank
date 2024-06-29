a781b75640d0da9ccb6558d876d6e73b99b3d013
karimov <usmonkarimov0726@gmail.com>
2024-06-29 14:01:40 +0500

    Created new functions to login and to create a new account

--- database.py
+++ database.py
@@ -24,19 +24,43 @@ class Dbase:
         self.conn=sqlite3.connect('bank.db')
         self.c = self.conn.cursor()
     
+    # Adds new user to the database
     def add_user(self,fullname,username,password,email,created_at):
-        self.c.execute("INSERT INTO users(full_name,username,password,email,created_at) VALUES(?,?,?,?,?)", (fullname,username,password,email,created_at))
-        self.conn.commit()
+        try:
+            self.c.execute("INSERT INTO users(full_name,username,password,email,created_at) VALUES(?,?,?,?,?)", (fullname,username,password,email,created_at))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(f'Database Error : {e}')
 
 
 
-
-
+    #Checks if provided  username or email is UNIQUE,else makes sure to get unique both of them
+    def check_exists(self,uname=True):
+        if uname:
+            while True:
+                name=input('Enter username : ')
+                self.c.execute("SELECT * FROM users WHERE username =? ",(name,))
+                data=self.c.fetchall()
+                if data:
+                    print(f' Username : {name} is not avaiable')
+                else:
+                    return name
+                    break
+        else:
+            while True:
+                email=input('Enter email : ')
+                self.c.execute("SELECT * FROM users WHERE email =? ",(email,))
+                data=self.c.fetchall()
+                if data:
+                    print(f' Email address : {email} is not avaiable')
+                else:
+                    return email
+                    break
 
 
 
     
-    
+    # Gets user's data from the database based on username, and checks validity with the password 
     def get_user_data(self,username,pas):
         try:
             self.c.execute("SELECT * FROM users WHERE username =?", (username,))
@@ -44,7 +68,7 @@ class Dbase:
             if data:
                 pass_hash = data[3]
                 if models.pas_check(pas,pass_hash):
-                    print(data)
+                    return data
                 else:
                     print('Invalid password or username')
             else:
--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import bcrypt 
+import database
 class Users:
     def __init__(self,full_name,username,password,email) -> None:
+        self.full_name = full_name
         self.username = username
         self.email=email
         salt = bcrypt.gensalt()
         self.password= bcrypt.hashpw(password.encode('utf-8'),salt)
-        self.full_name = full_name
         self.created_at=datetime.now().date()
     
 # Getting a password from a user for 1st time
@@ -21,7 +22,38 @@ def get_pass():
             mess='Wrong !!!'
     return pass1
 
+# Checking password hash from the database and user provided plaintext forn
 def pas_check(plaintext, hashed):
     return bcrypt.checkpw(plaintext.encode('utf-8'), hashed)
 
 
+
+# Funtion to create an account for a new user
+def create_acc():
+    # Creating a new database instance 
+    db = database.Dbase()
+
+    fname= input('Enter your fullname : ')
+    uname = db.check_exists()
+    email  =db.check_exists(False)
+    pas = get_pass()
+    #Creating Users Class with the provided inputs
+    user = Users(fname,uname,pas,email)
+    # Adding info of the user to the database
+    db.add_user(user.full_name,user.username,user.password,user.email,user.created_at)
+    db.close()
+    print('You have successfully created account')          
+
+
+def login():
+    db = database.Dbase()
+    print('Enter followinf credentials to login your account')
+    uname = input('Enter your username : ')
+    pas = input('Enter your password : ')
+    data = db.get_user_data(uname,pas)
+    if data:
+        print('You have successfuly logged into your account')
+        print(f'Full-name : {data[1]}')
+        print(f'Username : {data[2]}')
+        print(f'Email : {data[4]}')
+        print(f'Created-at: {data[5]}')
